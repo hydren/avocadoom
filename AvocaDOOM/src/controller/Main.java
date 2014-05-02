793743e2b9c1d303373d495d165ef68c0906fd36
hydren <5carlosfelipe5@gmail.com>
2014-05-02 17:16:41 +0000

    Updated changes.txt and notes.txt. Updated year in the licenses. Defaulted settings.

--- AvocaDOOM/src/controller/Main.java
+++ AvocaDOOM/src/controller/Main.java
@@ -1,5 +1,5 @@
 /*      
- *      Copyright 2012 Carlos F. M. Faruolo (aka Hydren) E-mail: 5carlosfelipe5@gmail.com
+ *      Copyright 2012-2014 Carlos F. M. Faruolo (aka Hydren) E-mail: 5carlosfelipe5@gmail.com
  *      
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
