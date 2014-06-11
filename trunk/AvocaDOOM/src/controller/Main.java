fa1ef18ba47a27a23e4f5fa14c91be9e9dbc7512
5carlosfelipe5@gmail.com <5carlosfelipe5@gmail.com@b0c3b1dd-40dd-2e1d-db4d-42567391bf52>
2014-06-11 05:20:09 +0000

    Possible fix of visual artifacts on scrolling.

--- trunk/AvocaDOOM/src/controller/Main.java
+++ trunk/AvocaDOOM/src/controller/Main.java
@@ -50,6 +50,7 @@ public class Main {
 	 */
 	public static void main(String[] args) 
 	{	
+		System.setProperty("sun.java2d.opengl","true"); //fix some visual artifacts
 		Settings settings = new Settings();
 		if(settings.useOSLF) try //TO FORCE TO USE THE SYSTEM LOOK & FEEL 
 		{
