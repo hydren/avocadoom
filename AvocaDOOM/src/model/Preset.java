e15f115b2c10b5bbbbf4eb658c744d0868561b39
hydren <5carlosfelipe5@gmail.com>
2014-06-17 16:22:19 +0000

    Replaced old Preset class with newer implementation and file storage.

--- AvocaDOOM/src/model/Preset.java
+++ AvocaDOOM/src/model/Preset.java
@@ -1,163 +1,119 @@
-/*      
- *      Copyright 2012 Carlos F. M. Faruolo (aka Hydren) E-mail: 5carlosfelipe5@gmail.com
- *      
- *      This program is free software; you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation; either version 2 of the License, or
- *      (at your option) any later version.
- *      
- *      This program is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
- *      
- *      You should have received a copy of the GNU General Public License
- *      along with this program; if not, write to the Free Software
- *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *      MA 02110-1301, USA.
- *      
- *      
- */
 package model;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
 import exceptions.FileAlreadyExistsException;
+import exceptions.InvalidPresetException;
 
 public class Preset 
 {
-	private static final String 
-	PRESET_VALIDATION_LABEL="[JAGGERY_PRESET]",
-	PRESET_WADS_LABEL="[WADS]",
-	PRESET_INFO_LABEL="[INFO]",
-	PRESET_NAME_TAG="name", 
-	PRESET_DESC_TAG="description",
-	PRESET_IMGPATH_TAG="image",
-	PRESET_ENGINES_TAG="engines";
-	
 	public File file;
 	public String name, description, imagePath;
-	public List<String> engines;
-	public List<Mod> mods;
+	public List<String> engines = new ArrayList<String>();;
+	public List<Mod> mods = new ArrayList<Mod>();
 	
-	public Preset(File file) throws FileNotFoundException, Exception
+	public Preset(File file) throws FileNotFoundException, IOException, InvalidPresetException
 	{
-		if( ! isValidPreset(file) ) throw new Exception("File is not a valid preset!");
-		else this.file = file;
+		if(file.isFile())
+			this.file = file;
 		load();
 	}
 	
-	public Preset(String filename) throws FileNotFoundException, Exception
+	public Preset(String filename) throws FileNotFoundException, IOException, InvalidPresetException
 	{
 		this(new File(filename));
 	}
 	
-	private void load() throws FileNotFoundException, Exception
+	private Preset() {}
+	
+	public static final String
+		PRESET_NAME_TAG="name", 
+		PRESET_ENGINES_TAG="engines",
+		PRESET_DESC_TAG="description",
+		PRESET_IMGPATH_TAG="image",
+		PRESET_MOD_PATH_TAG="mod.path";
+	
+	private void load() throws FileNotFoundException, IOException, InvalidPresetException
 	{
-		//starts a scanner for the file
-		Scanner scanner1 = new Scanner(file);
+		FileInputStream fis = new FileInputStream(file);
+		Properties p = new Properties();
 		
-		//if file is not empty, start parsing
-		if(scanner1.hasNextLine())
-		{	
-			//file needs to present this label for validation
-			if(scanner1.nextLine().equalsIgnoreCase(PRESET_VALIDATION_LABEL) )
-			{
-				//initialize to default values
-				this.name="<unnamed preset>";
-				this.description="<no description>";
-				this.imagePath = null;
-				this.mods = new ArrayList<Mod>();
-				this.engines = new ArrayList<String>();
-				String str=null;
-				
-				//needed when a token is accidently already in str variable
-				boolean alreadyRead = false;
-				
-				while(scanner1.hasNextLine())
-				{
-					//needed when a token is accidently already in str variable
-					if( ! alreadyRead ) str = scanner1.nextLine();
-					else alreadyRead = false;
-					
-					//label WADS found. Read a sequence of '@' prefixed paths
-					if(str.equalsIgnoreCase(PRESET_WADS_LABEL))
-					{
-						while(scanner1.hasNextLine())
-						{
-							str = scanner1.nextLine();
-							if(str.startsWith("@"))
-							{
-								mods.add(new Mod(str.substring(1)));
-							}
-							// it is another label found, need to set the alreadyRead flag because the label is already on the str variable
-							else if( str.startsWith("[") ) 
-							{
-								alreadyRead = true;
-								break;
-							}
-							else
-							{
-								//should I throw an exception???
-							}
-						}
-					}
-					
-					//label INFO found, read simple data
-					if(str.equalsIgnoreCase(PRESET_INFO_LABEL))
-					{
-						while(scanner1.hasNextLine())
-						{
-							str = scanner1.nextLine();
-							
-							if(str.trim().startsWith(PRESET_NAME_TAG) && str.contains("="))
-							{
-								this.name = str.substring(str.indexOf('=')+1).trim();
-							}
-							else if(str.trim().startsWith(PRESET_DESC_TAG) && str.contains("="))
-							{
-								this.description = str.substring(str.indexOf('=')+1).trim();
-							}
-							else if(str.trim().startsWith(PRESET_IMGPATH_TAG) && str.contains("="))
-							{
-								this.imagePath = str.substring(str.indexOf('=')+1).trim();
-							}
-							else if(str.trim().startsWith(PRESET_ENGINES_TAG) && str.contains("="))
-							{
-								String[] parts = str.substring(str.indexOf('=')+1).trim().split(",");
-								for(String s : parts) if( ! s.trim().equals("") ) this.engines.add( s.trim() );
-							}
-							// it is another label found, need to set the alreadyRead flag because the label is already on the str variable
-							else if( str.startsWith("[") ) 
-							{
-								alreadyRead = true;
-								break;
-							}
-							else
-							{
-								//should I throw an exception???
-							}
-						}
-					}
-				}
-				scanner1.close();
-				scanner1=null;
-				System.gc();
-			}
-			else throw new Exception("Invalid preset file \""+file.getName()+"\"! (at load() method)");	
-		}
-		else throw new Exception("Empty preset file \""+file.getName()+"\"! (at load() method)");
+		p.load(fis);
+		fis.close();
+		if(p.getProperty(PRESET_NAME_TAG)==null)
+			throw new InvalidPresetException("File "+file.getName()+" is not a valid preset! Missing name!");
+		
+		if( p.getProperty(PRESET_ENGINES_TAG)==null)
+			throw new InvalidPresetException("File "+file.getName()+" is not a valid preset! Missing engines!");
+		
+		name = p.getProperty(PRESET_NAME_TAG);
+		description = p.getProperty(PRESET_DESC_TAG);
+		imagePath = p.getProperty(PRESET_IMGPATH_TAG);
+		for(String s : p.getProperty(PRESET_ENGINES_TAG).split(","))
+			engines.add(s.trim());
+		
+		parsePaths();
 	}
 	
-	public void reload() throws FileNotFoundException, Exception
+	private void store() throws FileNotFoundException, IOException
 	{
-		load();
+		FileOutputStream fos = new FileOutputStream(file);
+		Properties p = new Properties();
+		p.setProperty(PRESET_NAME_TAG, name);
+		p.setProperty(PRESET_DESC_TAG, description);
+		p.setProperty(PRESET_IMGPATH_TAG, imagePath);
+
+		String engines_codes="";
+		for(String eng_code : engines)
+			engines_codes += eng_code+",";
+		if(engines_codes.length()>0) //if any engines added
+			engines_codes = engines_codes.substring(0, engines_codes.length()-1); //remove bogus last comma
+		p.setProperty(PRESET_ENGINES_TAG, engines_codes);
+		
+		p.store(fos, "avocadoom preset file v2");
+		fos.close();
+		fos=null;
+		System.gc();
+		storePaths();
+	}
+	
+	private void parsePaths() throws FileNotFoundException
+	{
+		Scanner scan = new Scanner(file);
+		
+		while(scan.hasNextLine())
+		{
+			String line = scan.nextLine();
+			if(line.trim().startsWith(PRESET_MOD_PATH_TAG) && line.contains("=") && line.indexOf('=') != line.length()-1)
+			{
+				String tmp = line.substring(line.indexOf('=')+1).trim();
+				if( !tmp.isEmpty() && Mod.isValidMod(tmp))
+					mods.add(new Mod(tmp));
+			}
+		}
+		
+		scan.close();
+	}
+	
+	private void storePaths() throws IOException
+	{
+		BufferedWriter w = new BufferedWriter(new FileWriter(file, true));
+		w.write("\n");
+		for(Mod mod : mods)
+			w.write(PRESET_MOD_PATH_TAG + "=" + mod.file.getPath()+"\n");
+		w.close();
+		w=null;
+		System.gc();
 	}
 	
 	public String getStatusText()
@@ -171,58 +127,69 @@ public class Preset
 		return name+"  -  "+file.getAbsolutePath();
 	}
 	
-	public static final void createAndSave(File file, String name, String desc, String imgPath, List<String> engines, List<Mod> modsPaths) throws IOException, FileAlreadyExistsException, Exception
+	public void reload() throws FileNotFoundException, IOException, InvalidPresetException
+	{
+		load();
+	}
+	
+	public static final 
+	void createAndSave(File file, String name, String desc, String imgPath, List<String> engines, List<Mod> modsPaths) throws IOException, FileAlreadyExistsException, Exception
 	{
 		createAndSave(file, name, desc, imgPath, engines, modsPaths, false);
 	}
 	
-	public static final void createAndSave(File file, String name, String desc, String imgPath, List<String> engines, List<Mod> modsPaths, boolean overwrite) throws IOException, FileAlreadyExistsException, Exception
+	public static final 
+	void createAndSave(File file, String name, String desc, String imgPath, List<String> engines, List<Mod> modsPaths, boolean overwrite) throws IOException, FileAlreadyExistsException, Exception
 	{
 		if(file.isDirectory()) throw new Exception("A folder with name "+file.getName()+" already exists!");
 		if(file.isFile() && ! overwrite) throw new FileAlreadyExistsException("A file with name "+file.getName()+" already exists!");
 		
 		file.createNewFile();
 		
-		FileWriter fw = new FileWriter(file);
-
-		fw.write(PRESET_VALIDATION_LABEL+'\n');
-		
-		fw.write(PRESET_INFO_LABEL+'\n');
-		fw.write(PRESET_NAME_TAG+'='+name+'\n');
-		fw.write(PRESET_DESC_TAG+'='+desc+'\n');
-		fw.write(PRESET_IMGPATH_TAG+'='+imgPath+'\n');
-		fw.write(PRESET_ENGINES_TAG+'=');
-		for(String str : engines) fw.write(str+",");
-		fw.write('\n');
-		
-		fw.write(PRESET_WADS_LABEL+'\n');
-		for(Mod m : modsPaths) fw.write('@'+m.file.getPath()+'\n');
-
-		fw.close();
-		fw=null;
-		System.gc();
+		Preset p = new Preset();
+		p.file = file;
+		p.description = desc;
+		p.engines = engines;
+		p.imagePath = imgPath;
+		p.mods = modsPaths;
+		p.name = name;
+		p.store();
 	}
 	
-	public static synchronized final boolean isValidPreset(File file) throws FileNotFoundException
+	public static synchronized final boolean isValidPreset(File file) throws IOException
 	{
-		
 		if(file == null) return false;
+		
+		//removed restriction. 
+		//TODO need a consensus about file extension...
+//		if( file.getName().toLowerCase().endsWith(".txt") == false ) return false;
 			
-		if( file.getName().toLowerCase().endsWith(".txt") ) //needs toLowerCase because sometimes people save as .TXT
-		{
-			Scanner scanner1 = new Scanner(file);
-			if(scanner1.hasNextLine()) if(scanner1.nextLine().equalsIgnoreCase(PRESET_VALIDATION_LABEL)) 
-			{
-				scanner1.close();
-				scanner1=null;
-				System.gc();
-				return true;
-			}
-			scanner1.close();
-			scanner1=null;
-			System.gc();
-		}
-		return false;
+		Scanner scanner1 = new Scanner(file);
+		if(scanner1.hasNextLine() == false) return false;
+		
+		if(scanner1.nextLine().equalsIgnoreCase("#avocadoom preset file v2") == false)
+			return false;
+		
+		scanner1.close();
+		scanner1=null;
+		System.gc();
+		
+		FileInputStream fis = new FileInputStream(file);
+		Properties p = new Properties();
+		
+		p.load(fis);
+		fis.close();
+		if(p.getProperty(PRESET_NAME_TAG)==null)
+			return false;
+		
+		if( p.getProperty(PRESET_ENGINES_TAG)==null)
+			return false;
+		
+		return true; //TODO add further validation
 	}
+	
+	//TODO
+	//convert old presets to new format
+	
 	
 }
